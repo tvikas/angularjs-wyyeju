dc51658973f07f76b68045844ced6be6b6b1b12b
tvikas <t.vikas@hotmail.com>
2021-11-30 18:39:31 +0530

    3 files modified

--- home/home.controller.js
+++ home/home.controller.js
@@ -1,8 +1,13 @@
 class HomeCtrl {
   constructor($scope) {
     'ngInject';
-
-    this.name = "AngularJS";
+    $scope.filterData = function (gender) {
+      alert(gender);
+    };
+    this.name = 'AngularJS';
+    this.data = JSON.parse(
+      '[{"id": 1,"first_name": "Jeanette","last_name": "Penddreth","email": "jpenddreth0@census.gov","gender": "Female"  }, {"id": 2,"first_name": "Giavani","last_name": "Frediani","email": "gfrediani1@senate.gov","gender": "Male"  }, {"id": 3,"first_name": "Noell","last_name": "Bea","email": "nbea2@imageshack.us","gender": "Female"  }, {"id": 4,"first_name": "Willard","last_name": "Valek","email": "wvalek3@vk.com","gender": "Male"  }]'
+    );
   }
 }
 
